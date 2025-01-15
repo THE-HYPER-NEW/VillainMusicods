71acfe28b4eba4172daf2e024bc627769ec3c18c
Bornhypersst <aniflixmusic@gmail.com>
2025-01-15 21:56:36 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="𝘽𝙤𝙧𝙣 𝙝𝙮𝙥𝙚𝙧",
+            url=f"https://t.me/BORNHYPER_SST",
         ),
     ],
 ]
