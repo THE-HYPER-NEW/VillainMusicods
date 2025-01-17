79b25fec691f7416b997a7ef32a926ae7d57e1c1
Bornhypersst <aniflixmusic@gmail.com>
2025-01-17 19:51:01 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -1,8 +1,8 @@
 import math
 from pyrogram.types import InlineKeyboardButton
-from PURVIMUSIC import app
+from SONALI import app
 import config
-from PURVIMUSIC.utils.formatters import time_to_seconds
+from SONALI.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
