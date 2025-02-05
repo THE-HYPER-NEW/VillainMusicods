9c4039bf68bd56a79c1801d323dcb2161e757032
Bornhypersst <aniflixmusic@gmail.com>
2025-02-05 21:00:31 +0530

    Update approve.py

--- SONALI/plugins/bot/approve.py
+++ SONALI/plugins/bot/approve.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/radha_music_bot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/SSTXMUSIC_BOT?startgroup=true"),
     ],
 ]
 
